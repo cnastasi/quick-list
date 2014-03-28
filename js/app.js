f2ed503f207e0c35b780df0974f83ed69828acf8
predo <predorock@gmail.com>
2014-03-28 12:22:15 +0100

    Service per il localstorage e memorizzazione dati

--- js/app.js
+++ js/app.js
@@ -1,30 +1,58 @@
 var app = angular.module("FirefoxOSApp", []);
 
-app.controller("TodoController",function($scope){
-   $scope.todos = [
-    {
-      text:'learn angular', 
-      done:true
-    },{
-      text:'build an angular app', 
-      done:false
-    }];
-     
+
+app.factory("StorageService",function(){
+  return{
+    set: function(key,value){
+        localStorage[key] = JSON.stringify(value);
+    },
+    get: function(key){
+      if(localStorage[key])
+        return JSON.parse(localStorage[key]);
+      else
+        throw "invalid key " + key; 
+    },
+    remove: function(key){
+      if(localStorage[key])
+        delete localStorage[key];
+      else
+        throw "invalid key " + key;
+    }
+  };
+});
+
+app.factory("TodoListService",function($scope,StorageService){
+  return{
+    save: function(){
+        StorageService.set("TodoList",$scope.todos);
+    },
+    load: function(){
+        StorageService.get("TodoList");
+    }
+  };
+});
+
+app.controller("TodoController",function($scope,TodoListService){
+    
     $scope.addTodo = function() {
       $scope.todos.push({text:$scope.todoText, done:false});
       $scope.todoText = '';
+      TodoListService.save();
     };
+
     $scope.archive = function() {
       var oldTodos = $scope.todos;
       $scope.todos = [];
       angular.forEach(oldTodos, function(todo) {
         if (!todo.done) $scope.todos.push(todo);
       });
+      TodoListService.save();
     };
-    
+
     $scope.deleteTodo = function($index) {
       console.log($index);
       $scope.todos.splice($index,1);
+      TodoListService.save();
     };
 
 });
